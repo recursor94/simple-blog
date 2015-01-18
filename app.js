4a583c506624f3d0380a2161b777a87eecd680af
Andrew <werdnaonaps@gmail.com>
2015-01-18 16:05:14 -0500

    setup database and gave proper authentification information

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+var Sequelize = require('sequelize')
+
+  , sequelize = new Sequelize('article', 'postgres', 'cococat9', {
+      dialect: "postgres", // or 'sqlite', 'postgres', 'mariadb'
+      port:    5432, // or 5432 (for postgres)
+    });
+ 
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database:', err);
+    } else {
+      console.log('Connection has been established successfully.');
+    }
+  });
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
